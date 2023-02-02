20522a5ff53e4ebd4e68d3a8f7680d47716db00f
scheisa <starostindenis710@gmail.com>
2023-02-02 21:56:17 +0200

    theme improvement

--- themes.js
+++ themes.js
@@ -3,45 +3,38 @@ let trooper = document.querySelector('.trooper')
 let gruvbox = document.querySelector('.gruvbox')
 let def = document.querySelector('.default')
 
-const theme = localStorage.getItem('currentTheme');
+const Theme = localStorage.getItem('currentTheme');
 updateUI();
 
 function updateUI(){
-  if (theme == "yoda") {
-    document.body.style.setProperty("--bg", "#2B2A33")
-    document.body.style.setProperty("--accent", "#4a5334")
-    document.body.style.setProperty("--second-accent", "#573e37")
-    document.getElementsByClassName("image")[0].src = "pictures/yoda.jpg";
-  }
-  else if (theme == "trooper") {
-    document.body.style.setProperty("--bg", "#2B2A33")
-    document.body.style.setProperty("--accent", "white")
-    document.body.style.setProperty("--second-accent", "black")
-    document.getElementsByClassName("image")[0].src = "pictures/trooper.jpg";
-  }
-  else if (theme == "gruvbox") {
-    document.body.style.setProperty("--bg", "#2B2A33")
-    document.body.style.setProperty("--accent", "#628912")
-    document.body.style.setProperty("--second-accent", "#cb8c18")
-    document.getElementsByClassName("image")[0].src = "pictures/gruvbox.jpg";
-  }
+	if (Theme == "yoda") {
+		document.body.style.setProperty("--bg", "#2B2A33")
+		document.body.style.setProperty("--accent", "#4a5334")
+		document.body.style.setProperty("--second-accent", "#573e37")
+		document.getElementsByClassName("image")[0].src = "pictures/yoda.jpg";
+	}
+	else if (Theme == "trooper") {
+		document.body.style.setProperty("--bg", "#2B2A33")
+		document.body.style.setProperty("--accent", "white")
+		document.body.style.setProperty("--second-accent", "black")
+		document.getElementsByClassName("image")[0].src = "pictures/trooper.jpg";
+	}
+	else if (Theme == "gruvbox") {
+		document.body.style.setProperty("--bg", "#2B2A33")
+		document.body.style.setProperty("--accent", "#628912")
+		document.body.style.setProperty("--second-accent", "#cb8c18")
+		document.getElementsByClassName("image")[0].src = "pictures/gruvbox.jpg";
+	}
 }
 
-def.addEventListener('click' ,()=>{
-  localStorage.setItem("currentTheme" , "")
-});
+function ThemeToggle(theme, strTheme) {
+	theme.addEventListener('click' ,()=>{
+		localStorage.setItem("currentTheme" , strTheme)
+	});
+	console.log(strTheme)
+}
 
-yoda.addEventListener('click',()=>{
-  localStorage.setItem("currentTheme" , "yoda")
-  updateUI();
-});
-
-trooper.addEventListener('click',()=>{
-  localStorage.setItem("currentTheme" , "trooper")
-  updateUI();
-});
-
-gruvbox.addEventListener('click',()=>{
-  localStorage.setItem("currentTheme" , "gruvbox")
-  updateUI();
-});
+ThemeToggle(trooper, "trooper");
+ThemeToggle(yoda, "yoda");
+ThemeToggle(gruvbox, "gruvbox");
+ThemeToggle(def, "");
